85ebec0798e5d770011fc749f8a109bf8c798822
gh0st <maratka2202@gmail.com>
2022-09-10 12:32:08 +0300

    Create output custom text with mini-animation (delay)

---
+++ give_love.py
@@ -0,0 +1,38 @@
+import os
+
+from time import sleep
+from colorama import init, Fore, Style
+
+COLOR = Fore.MAGENTA  # text color in terminal
+STYLE = Style.BRIGHT
+
+# users
+USER1 = "u1"
+USER2 = "u2"
+
+init(autoreset=True)  # init colorama
+
+
+def clear_screen() -> None:
+    """Clear screen based on user system."""
+    return
+
+
+def main() -> None:
+    clear_screen()  # clean screen for aesthetic output
+
+    text: list = [
+        "============================================================================",
+        f"|                  @{USER1} очень любит тебя, @{USER2}!                  |",
+        "============================================================================"
+    ]
+
+    for text_line in text:
+        print("\n")
+        for char in text_line:
+            print(COLOR + STYLE + char, flush=True, end="")
+            sleep(.1)
+
+
+if __name__ == "__main__":
+    main()
